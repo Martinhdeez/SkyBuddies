5f620604d5ea1491ed2fac8a0d175b7932eecac4
alex-mosquera-gundin <alex.mosquera@udc.es>
2025-05-03 03:48:23 +0200

    feat: group chat functionality

--- backend/app/db/repositories/chat_repository.py
+++ backend/app/db/repositories/chat_repository.py
@@ -9,17 +9,6 @@ class ChatRepository(Repository):
     def __init__(self):
         super().__init__(chats_collection, convert_chat_to_model)
 
-    async def get_chat_by_uid(self, uid: str) -> Chat | None:
-        chat = await self.data_collection.find_one(
-            {
-                "sender_uid": uid
-            }
-        )
-        if not chat:
-            return None
-
-        return self.convert_helper(chat)
-
     async def get_chat_by_chat_id(self, chat_id: str) -> Chat | None:
         chat = await self.data_collection.find_one(
             {
@@ -30,17 +19,6 @@ class ChatRepository(Repository):
             return None
 
         return self.convert_helper(chat)
-
-
-    async def get_all_chats_by_uid(self, uid: str) -> List[Chat]: 
-        return self.data_collection.find({
-            "sender_uid": uid
-        }).sort("updated_at", -1)
-
-    async def get_n_chats_by_uid(self, uid: str, n: int, incr: int) -> List[Chat]: 
-        return self.data_collection.find({
-            "sender_uid": uid
-        }).sort("updated_at", -1).limit(n*incr)
 
     async def add_message(self, chat_id: str, message_id: str):
         await self.data_collection.update_one(
--- backend/app/services/chat_service.py
+++ backend/app/services/chat_service.py
@@ -11,23 +11,8 @@ class ChatService:
         chat_model.created_at = datetime.now()
         return await self.repository.add_data(chat_model)
 
-    async def get_chat_by_uid(self, uid: str) -> Chat | None:
-        return await self.repository.get_chat_by_uid(uid)
-
     async def get_chat_by_chat_id(self, chat_id: str):
         return await self.repository.get_chat_by_chat_id(chat_id)
-
-    async def get_all_chats_by_id(self, uid: str):
-        return await self.repository.get_all_chats_by_uid(uid)
-
-    async def get_n_chats_by_id(self, uid: str, n: int, incr: int):
-        return await self.repository.get_n_chats_by_uid(uid, n, incr)
-
-    async def get_chat_by_id(self, chat_id: str):
-        chat = await self.repository.get_data(chat_id)
-        if not chat:
-            raise ValueError("Chat not found")
-        return chat
 
     async def add_message_to_chat(self, chat_id: str, message_id: str):
         await self.repository.add_message(chat_id, message_id)
