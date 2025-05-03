c9c427946a542d2b5da76be64962f21dafdf0175
Martinhdeez <m.hernandezg@udc.es>
2025-05-03 10:05:56 +0200

    fix: add visibility to group

--- backend/app/db/models/group.py
+++ backend/app/db/models/group.py
@@ -4,9 +4,14 @@ from datetime import datetime
 from enum import Enum
 from uuid import UUID
 
+class GroupVisibility(str, Enum):
+    PUBLIC = "public"
+    PRIVATE = "private"
+
 class Group(BaseModel):
     id: Optional[str] = Field(default=None, title="id")
     name: str
     members: List[str]
+    visibility: Optional[GroupVisibility] = Field(default=GroupVisibility.PUBLIC, title="visibility")
     updated_at: datetime = Field(default=datetime.now(), title="updated_at")
     created_at: datetime = Field(default=datetime.now(), title="created_at")
--- backend/app/routers/dto/group_dto.py
+++ backend/app/routers/dto/group_dto.py
@@ -1,9 +1,11 @@
 from typing import Optional, Text
 from pydantic import BaseModel, EmailStr, Field
+from db.models.group import GroupVisibility
 
 class CreateGroupDTO(BaseModel):
     name: Text
     members: Optional[list[str]] = Field(default=list, title="Members of the group")
+    visibility: Optional[GroupVisibility] = Field(default=GroupVisibility.PUBLIC, title="Visibility of the group")
 
 
 class MembersGroupDTO(BaseModel):
