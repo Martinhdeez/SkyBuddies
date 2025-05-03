90028694f7d1cd37b95f2e42f70f622de5ea0037
alex-mosquera-gundin <alex.mosquera@udc.es>
2025-05-03 09:12:09 +0200

    fix: the list of countries that matched with filters powered by gemini now is a entirely python list

--- backend/app/services/gemini_service.py
+++ backend/app/services/gemini_service.py
@@ -18,7 +18,7 @@ class GeminiService:
             )
             
             if response.text:
-                return response.text
+                return self._parse_response(response.text)
             else:
                 print("Empty response from Gemini.")
                 return None
@@ -27,12 +27,26 @@ class GeminiService:
             return None
     
     def _build_prompt(self, filters: dict) -> str:
-        prompt = "Based on the following filters, recommend 10 countries to visit in order of best match:"
+        prompt = "Based on the following filters, recommend 10 countries to visit in order of best match:\n"
         for key, value in filters.items(): 
-            prompt += f"\n- {key}: {value}"
-        prompt += "\n\nPlease provide the recommendations only in a list format exactly "
-        "like this: ['Spain', 'Germany', 'Italy', ...] (without any other message)."
-        "In the list, the firsts countries should be the best match."
-        prompt += "\n\nPlease do not include any other text or explanation, just the list of countries. (don't include \n)"
-        prompt += "The match should be based on the filters provided."
+            prompt += f"- {key}: {value}\n"
+        prompt += "\nPlease provide the recommendations in the following format, with a number before each country: "
+        prompt += "`1. Spain\n2. Germany\n3. Italy ...` (where each country is preceded by its ranking number, starting from 1)."
+        prompt += "\n\nThe countries should be ordered by best match with the filters provided."
+        prompt += "\nPlease do not include any other text or explanation, just the list of countries in this exact format."
+        prompt += " Do not include any other symbols, like dashes or bullets. Only numbers and country names."
         return prompt
+
+    
+    # format: 1. Spain\n2. Germany\n3. Italy...
+    def _parse_response(self, response: str) -> list:
+        try: 
+            response = response.replace("-", "").strip()
+            countries = response.split("\n")
+            countries = [country.strip() for country in countries if country]  
+            countries = [country.split(". ")[1] if ". " in country else country for country in countries]
+            
+            return countries
+        except Exception as e:
+            print(f"Error parsing response: {e}")
+            return []
